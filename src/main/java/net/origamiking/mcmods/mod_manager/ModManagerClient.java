963f720249f32bcd04ac58f6af7a87180ab61879
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-22 20:29:55 -0500

    Initial commit

---
+++ src/main/java/net/origamiking/mcmods/mod_manager/ModManagerClient.java
@@ -0,0 +1,11 @@
+package net.origamiking.mcmods.mod_manager;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModManagerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
