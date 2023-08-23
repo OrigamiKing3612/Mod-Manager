47c277477c685f215a6c07b5167b71e70f36fbab
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-22 20:41:03 -0500

    Initial commit

--- src/main/java/net/origamiking/mcmods/mod_manager/ModManager.java
+++ src/main/java/net/origamiking/mcmods/mod_manager/ModManager.java
@@ -1,22 +1,15 @@
 package net.origamiking.mcmods.mod_manager;
 
-import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModManager implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("mod_manager");
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+public class ModManager implements ClientModInitializer {
+    public static final String MOD_ID = "mod_manager";
+    public static final String VERSION = "0.0.1-1.20.1";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitializeClient() {
+        ModManager.LOGGER.info("Starting Mod-Manager " + ModManager.VERSION);
+    }
 }
