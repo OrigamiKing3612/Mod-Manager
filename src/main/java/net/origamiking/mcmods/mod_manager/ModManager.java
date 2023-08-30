8d15439d5d114e6aa14257453d5ed1ac8cb563a3
OrigamiKing3612 <origamiking3612@gmail.com>
2023-08-29 21:02:38 -0500

    Todos

--- src/main/java/net/origamiking/mcmods/mod_manager/ModManager.java
+++ src/main/java/net/origamiking/mcmods/mod_manager/ModManager.java
@@ -12,6 +12,12 @@ public class ModManager implements ClientModInitializer {
 
     //TODO Icon?
     //TODO better browse
+    //TODO Datapacks
+    //TODO other ways to get into the ChooseScreen V
+        //keybind?
+        //resource packs screen
+        //datapacks screen
+        //shaders screen w/sodium and better sodium menu
 
 
     @Override
